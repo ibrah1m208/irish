d236829e216f5e69a71109f7a68d570f50aec9f4
ibrah1m208 <md.ibrahim.bin.faisal@gmail.com>
2026-08-20 23:49:59 +0530

    Make working shell prompt - format prompt path - compare current dir and the home dir - print prompt - prints <uname@host:cwd> correctly in the terminal
    - Make naive user input with char array of fixed length and scanf
    TODO FIX - USE getline, use better coding practices (#define INPUT_SIZE 256 //for ex)
    
    Done A1

--- c-shell/src/main.c
+++ c-shell/src/main.c
@@ -1,7 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pwd.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<pwd.h>
+#include<unistd.h>
+#include<limits.h>
+#include<stdlib.h>
+#include<string.h>
+
+char *format_prompt_path(const char *cwd, const char *home) {
+    size_t home_len = strlen(home);
+    if (strncmp(cwd, home, home_len) == 0) {
+        char next_char = cwd[home_len];
+
+        if (next_char == '\0') { // Check if cwd is $HOME
+            return strdup("~");
+        }
+        if (next_char == '/') { // Check if $HOME is ancestor to cwd
+            char *result = malloc(1 + strlen(cwd + home_len) + 1);
+            result[0] = '~';
+            strcpy(result + 1, cwd + home_len);
+            return result;
+        }
+    }
+    // Else
+    return strdup(cwd);
+}
+
+void print_prompt(const char *home_dir) {
+    struct passwd *pw = getpwuid(getuid()); //i208
+    char hostname[256];
+    char cwd[PATH_MAX];
+
+    gethostname(hostname, sizeof(hostname)); //ASUSVivobook
+    getcwd(cwd, sizeof(cwd));
+
+    char *display_path = format_prompt_path(cwd, home_dir);
+
+    printf("<%s@%s:%s> ", pw ? pw->pw_name : "unknown", hostname, display_path);
+    fflush(stdout); //flush any unsolicited "\n"
+
+    free(display_path);
+}
 
 int main(int argc, char *argv[]) {
     (void)argc;
@@ -9,17 +47,15 @@ int main(int argc, char *argv[]) {
     
     /* TODO: Implement loop */
     printf("initiate C shell\n\n\n");
-    
-    /* Temporary: Show current user and directory */
-    struct passwd *pw = getpwuid(getuid()); 
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) == NULL) {
-        perror("getcwd");
-        return 1;
+    char *session_home_dir = getcwd(NULL, 0);
+    int runtime = 1;
+    while(runtime) {
+        print_prompt(session_home_dir);
+        char input[256];
+        scanf("%s", input);
+        if(strncmp("exit", input, 4) == 0) runtime = 0;
     }
-    
-    printf("User: %s, current dir: %s\n", pw ? pw->pw_name : "unknown", cwd);
-    printf("Press to Continue...\n");
-    
+
+    free(session_home_dir);
     return 0;
 }
